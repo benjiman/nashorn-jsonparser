b2beea67ccdcca6a40ac503109f3be96cff03620
benji <git@benjiweber.com>
2016-10-22 15:35:02 +0100

    Clarify JS code a bit

--- src/main/resources/com/benjiweber/nashorn/jsonparser/jsonparser.js
+++ src/main/resources/com/benjiweber/nashorn/jsonparser/jsonparser.js
@@ -1,41 +1,61 @@
-function wrapInFunction(value) function() value;
-function isArray(value) typeof value == 'object' && typeof value.length != 'undefined';
-function isSimpleValue(value) typeof value.class != 'undefined';
-function typeForComplexValue(key, containerType) Packages.java.lang.Class.forName(containerType).getMethod(key).getReturnType().getName()
-function typeForComplexArrayValue(key, containerType) Packages.java.lang.Class.forName(containerType).getMethod(key).getGenericReturnType().getActualTypeArguments()[0].getName();
+var classByName = Packages.java.lang.Class.forName
+var List = Packages.java.util.ArrayList
+function wrapInFunction(value) function() value
+
+function isArray(value)
+    typeof value === 'object' &&
+    typeof value.length !== 'undefined'
+
+function isSimpleValue(value) typeof value.class !== 'undefined'
+
+function typeForObjectValue(key, containerType)
+    classByName(containerType)
+        .getMethod(key)
+        .getReturnType()
+        .getName()
+
+function typeForObjectArrayValue(key, containerType)
+    classByName(containerType)
+        .getMethod(key)
+        .getGenericReturnType()
+        .getActualTypeArguments()[0]
+        .getName()
 
 function wrapValuesInFunctions(map) {
     var wrapped = {}
-    for (var key in map) wrapped[key] = wrapInFunction(map[key]);
-    return wrapped;
+    for (var key in map) wrapped[key] = wrapInFunction(map[key])
+    return wrapped
+}
+
+function containsObjects(array) array.length > 0 && !isSimpleValue(array[0])
+
+function parseObjectArray(array, key, type) {
+    return new List(
+        array.map(function(item) parse(
+            typeForObjectArrayValue(key, type),
+            JSON.stringify(item)
+        ))
+    )
+}
+
+function parseProperty(type, parsedJSON, property) {
+    if (isArray(property.value)) {
+        return containsObjects(property.value)
+                ? parseObjectArray(property.value, property.name, type)
+                : new List(property.value)
+    } else if (!isSimpleValue(property.value)) {
+        return parse(typeForObjectValue(property.name, type), JSON.stringify(property.value))
+    } else {
+        return property.value
+    }
 }
 
 function parse(type, json) {
-
-    var complexValues = {}
-    var parsed = JSON.parse(json);
+    var parsed = JSON.parse(json)
 
     for (var key in parsed) {
-        if (isArray(parsed[key])) {
-            if (parsed[key].length > 0 && isSimpleValue(parsed[key][0])) {
-                complexValues[key] = new Packages.java.util.ArrayList(parsed[key]);
-            } else {
-                var array = [];
-                for each (value in parsed[key]) {
-                       array.push(parse(typeForComplexArrayValue(key, type), JSON.stringify(value)))
-                }
-                complexValues[key] = new Packages.java.util.ArrayList(array);
-            }
-            delete parsed[key];
-        } else if (!isSimpleValue(parsed[key])) {
-            complexValues[key] = parse(typeForComplexValue(key, type), JSON.stringify(parsed[key]))
-            delete parsed[key]
-        }
-    }
-    var simpleValues = wrapValuesInFunctions(parsed);
-    for (var key in complexValues) {
-        simpleValues[key] = wrapInFunction(complexValues[key]);
+        parsed[key] = parseProperty(type, parsed, { name: key, value: parsed[key] })
     }
 
-    return new Packages[type](simpleValues)
-};
+    return new Packages[type](wrapValuesInFunctions(parsed))
+}
